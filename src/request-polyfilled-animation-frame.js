74913ca9bdf6e1474856cbea4a83c26f17a13d79
Irakli Gozalishvili <rfobic@gmail.com>
2016-11-03 17:07:15 -0700

    Annotate export variables
    Flow seems to generate incorrect type annotations facebook/flow#2747 when export type is inferred, so we workaround by annotating export itself.

--- src/request-polyfilled-animation-frame.js
+++ src/request-polyfilled-animation-frame.js
@@ -2,52 +2,53 @@
 
 import performance$now from 'performance-now'
 
-type Request = (timestamp:number) => void
+type Request = (callback:(timestamp:number) => void) => number
+type Cancel = (id:number) => void
 
 export const now:() => number = performance$now
 
 let lastTime = now()
 
-export const requestPolyfilledAnimationFrame =
-  (callback:Request):number => {
+export const requestPolyfilledAnimationFrame:Request =
+  callback => {
     const time = now()
     const remaining = Math.max(0, 16 - (time - lastTime))
     lastTime = time + remaining
     return setTimeout(callback, remaining, lastTime)
   }
 
-export const cancelPolyfilledAnimationFrame =
-  (id:number):void =>
+export const cancelPolyfilledAnimationFrame:Cancel =
+  id =>
   clearTimeout(id)
 
-export const requestAnimationFrame = typeof window === 'undefined'
+export const requestAnimationFrame:Request = typeof window === 'undefined'
   ? requestPolyfilledAnimationFrame
   : typeof window.msRequestAnimationFrame === 'function'
-  ? (callback:Request):number => window.msRequestAnimationFrame(callback)
+  ? callback => window.msRequestAnimationFrame(callback)
   : typeof window.mozRequestAnimationFrame === 'function'
-  ? (callback:Request):number => window.mozRequestAnimationFrame(callback)
+  ? callback => window.mozRequestAnimationFrame(callback)
   : typeof window.webkitRequestAnimationFrame === 'function'
-  ? (callback:Request):number => window.webkitRequestAnimationFrame(callback)
+  ? callback => window.webkitRequestAnimationFrame(callback)
   : typeof window.oRequestAnimationFrame === 'function'
-  ? (callback:Request):number => window.oRequestAnimationFrame(callback)
+  ? callback => window.oRequestAnimationFrame(callback)
   : requestPolyfilledAnimationFrame
 
-export const cancelAnimationFrame = typeof window === 'undefined'
+export const cancelAnimationFrame:Cancel = typeof window === 'undefined'
   ? cancelPolyfilledAnimationFrame
   : typeof window.msCancelAnimationFrame === 'function'
-  ? (id:number):void => window.msCancelAnimationFrame(id)
+  ? id => window.msCancelAnimationFrame(id)
   : typeof window.mozCancelAnimationFrame === 'function'
-  ? (id:number):void => window.mozCancelAnimationFrame(id)
+  ? id => window.mozCancelAnimationFrame(id)
   : typeof window.webkitCancelAnimationFrame === 'function'
-  ? (id:number):void => window.webkitCancelAnimationFrame(id)
+  ? id => window.webkitCancelAnimationFrame(id)
   : typeof window.oCancelAnimationFrame === 'function'
-  ? (id:number):void => window.oCancelAnimationFrame(id)
+  ? id => window.oCancelAnimationFrame(id)
   : typeof window.msCancelRequestAnimationFrame === 'function'
-  ? (id:number):void => window.msCancelRequestAnimationFrame(id)
+  ? id => window.msCancelRequestAnimationFrame(id)
   : typeof window.mozCancelRequestAnimationFrame === 'function'
-  ? (id:number):void => window.mozCancelRequestAnimationFrame(id)
+  ? id => window.mozCancelRequestAnimationFrame(id)
   : typeof window.webkitCancelRequestAnimationFrame === 'function'
-  ? (id:number):void => window.webkitCancelRequestAnimationFrame(id)
+  ? id => window.webkitCancelRequestAnimationFrame(id)
   : typeof window.oCancelRequestAnimationFrame === 'function'
-  ? (id:number):void => window.oCancelRequestAnimationFrame(id)
+  ? id => window.oCancelRequestAnimationFrame(id)
   : cancelPolyfilledAnimationFrame
